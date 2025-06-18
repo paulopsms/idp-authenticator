36f47e046abb7b495fa977c95d51e3c63d4fecc7
paulopsms <paulopsms@gmail.com>
2025-06-17 23:32:28 -0300

    First commit

---
+++ src/main/java/com/paulopsms/idp_authenticator/IdpAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.paulopsms.idp_authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdpAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdpAuthenticatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulopsms/idp_authenticator/IdpAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulopsms.idp_authenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdpAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
